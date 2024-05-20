e583e60cac5ff27c757d4c6e7d8de31a87629d78
Pedro Leite <pedroafonsoleite11@gmail.com>
2024-05-20 17:49:41 -0300

    ner

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div className='border-b-2 ml-[200px]'>
+        <div>
+          <img className='h-16' src="https://svgur.com/i/16Fa.svg" alt="" />
+        </div>
+      </div>
+      <div className=''>
+        <div className='border-b-2 flex justify-center'>
+          <p>
+            dust
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <div className="">
+            <img className='w-[500px] h-[340px]' src="./assets/IV.png" alt="" />
+            <p>
+              Metal Slug: Uma Odisseia Explosiva Pelos
+              Corredores da Nostalgia
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
